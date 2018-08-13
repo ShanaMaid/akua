d16af76864750cec3ea29a7bc475886f1c140dcf
ShanaMaid <uestczeng@gmail.com>
2018-08-13 09:41:29 +0800

    clear

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-export default class Akua {
-    private stack;
-    private separator;
-    private isValidTag;
-    checkParent(parent: string): boolean;
-    parse(): this;
-    inject(condition: boolean, tag: string, func: () => void): this;
-    constructor();
-}
